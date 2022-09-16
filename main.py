6f6228f0abf3a7395046e04dae748cb2ccfe1d4a
Niyaz <niazlv03@gmail.com>
2022-09-16 06:50:00 +0000

    update design and many bug fix

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from pytils import numeral
 
 import vpnconfig
 import database
-from config import *
+import config
 
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config.token)
 db = database.databaseVPN()
 user_data = {}
 isReal_payment = True
@@ -31,38 +31,112 @@ def is_digit(string):
             return False
 
 def get_price(hour:int):
-    start_fee = price * 0.03 #3% от суммы
+    start_fee = config.price * 0.03 #3% от суммы
     if(hour <=720):
         fee = start_fee - ((start_fee/720)*hour)
     else:
         fee = 0
-    return str(round(((price/720)*hour)+fee,1))
+    return str(round(((config.price/720)*hour)+fee,1))
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.InlineKeyboardMarkup()
     item1=types.InlineKeyboardButton(text="Начать!",callback_data="start")
     markup.add(item1)
-    bot.send_message(message.chat.id,"Привет ✌️\nЯ предоставляю VPN сервисы за маленькую стоимость(просто поддержание работы сервиса и оплату хостинга).",reply_markup=markup)
+    bot.send_message(message.chat.id,
+                    "Привет ✌️\n"
+                    "Я предоставляю VPN сервисы за маленькую стоимость(просто поддержание работы сервиса и оплату хостинга)."
+                    ,reply_markup=markup)
 
 @bot.message_handler(commands=['menu'])
 def menu_message(message):
-    class XClass(object):
-        def __init__(self):
-            self.message = message
-            self.data = 'main start'
+    resp = db.getUser("userid",message.chat.id)
+    if(len(resp)<=0):
+        class XClass(object):
+            def __init__(self):
+                self.message = message
+                self.data = 'start#'
+    else:
+        msg = bot.send_message(message.chat.id,"*menu*")
+        class XClass(object):
+            def __init__(self):
+                self.message = msg
+                self.data = 'main#'
     callback_inline(XClass())
 
-def getvpnstr(userid:str)->str:
+@bot.message_handler(commands=['refill_balance'])
+def refill_balance_message(message):
+    resp = db.getUser("userid",message.chat.id)
+    if(len(resp)<=0):
+        class XClass(object):
+            def __init__(self):
+                self.message = message
+                self.data = 'start#'
+    else:
+        #msg = bot.send_message(message.chat.id,"*refill_balance*")
+        class XClass(object):
+            def __init__(self):
+                self.message = message
+                self.data = 'refill#'
+    callback_inline(XClass())
+
+@bot.message_handler(commands=['account'])
+def refill_balance_message(message):
+    resp = db.getUser("userid",message.chat.id)
+    if(len(resp)<=0):
+        class XClass(object):
+            def __init__(self):
+                self.message = message
+                self.data = 'start'
+    else:
+        msg = bot.send_message(message.chat.id,"*account*")
+        class XClass(object):
+            def __init__(self):
+                self.message = msg
+                self.data = 'account#'
+    callback_inline(XClass())
+
+@bot.message_handler(commands=['vpn'])
+def vpn_message(message):
+    resp = db.getUser("userid",message.chat.id)
+    if(len(resp)<=0):
+        class XClass(object):
+            def __init__(self):
+                self.message = message
+                self.data = 'start'
+    else:
+        msg = bot.send_message(message.chat.id,"*vpn*")
+        class XClass(object):
+            def __init__(self):
+                self.message = msg
+                self.data = 'vpn#'
+    callback_inline(XClass())
+
+def getvpnstr(userid:str, detalizated=False)->str:
     result = ""
     try:
+        # получить точную активацию
+        # vpns = db.execute("SELECT time_delete, user, vpnid, status FROM vpns WHERE userid = "+userid)
+        # _json = json.loads(vpns[0][3])
+        # _json['status']['value']
+        # _json['status']['description']
         data = db.getUser("userid",userid)[0]
         jsonfromdata = json.loads(data[9])
         for i in range(0,len(jsonfromdata['vpnid'])):
             vpns = db.getVpn(jsonfromdata['vpnid'][i])[0]
-            result += str(i+1)+".\t\tlogin: "+vpns[3] + "\n\t\tpasswd: "+vpns[4]+"\n\t\tserver/ip: "+vpns[1]+"\n\t\tprotocol: "+vpns[2]+"\n\t\tPSK: "+vpnconfig.getPSK()+"\n\t\tdestruction time: "+vpns[5]+"\n\n"
+            result += str(i+1)+".\t\tlogin: "+vpns[3]
+            if(detalizated):
+                result += "\n\t\tpasswd: "+vpns[4]
+                result += "\n\t\tserver/ip: "+vpns[1]
+                result += "\n\t\tprotocol: "+vpns[2]
+                result += "\n\t\tPSK: "+vpnconfig.getPSK()
+            _datedelta = datetime.datetime.now() - datetime.datetime.strptime(vpns[5],('%Y-%m-%d %H:%M:%S'))
+            _strdate = (numeral.get_plural((_datedelta.days*-1), "день, дня, дней")) if _datedelta < datetime.timedelta(seconds=1) else "Expired"
+            # result += "\n\t\tdestruction time: "+vpns[5]+"("+_strdate+")"
+            result += "\n\t\tdestruction time: "+_strdate
+            result += "\n\n"
     except Exception as e:
-        print(data)
+        print(e)
     return result
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -73,12 +147,14 @@ def callback_inline(call):
     destroy_btn = types.InlineKeyboardButton(text="Закрыть",callback_data="destroy")
     main_btn = types.InlineKeyboardButton(text="В главное меню",callback_data="main")
     refill_btn = types.InlineKeyboardButton(text="Пополнить баланс", callback_data="refill")
+    detalizated_vpns_btn = types.InlineKeyboardButton(text="Подробные данные", callback_data="detalizated_account")
     # Если сообщение из чата с ботом
     if call.message:
-        if call.data == "start":
+        if call.data[:5] == "start":
             resp = db.getUser("userid",call.message.chat.id)
             if(len(resp)<=0):
-                bot.answer_callback_query(call.id)
+                if(not call.data[5:6] == "#"):
+                    bot.answer_callback_query(call.id)
                 BTNmarkup = types.ReplyKeyboardMarkup(True)
                 item1=types.KeyboardButton("Принять")
                 BTNmarkup.add(item1)
@@ -91,14 +167,26 @@ def callback_inline(call):
                 bot.answer_callback_query(call.id)
             markup.add(refill_btn,account_btn)
             markup.add(vpn_btn)
-            markup.add(destroy_btn)
+            #markup.add(destroy_btn)
             text = "Вы в главном меню!\nОтсюда вы можете попасть в любое из подменю бота!"
             if(call.data == "main start"):
                 bot.send_message(call.message.chat.id,text,reply_markup=markup)
             else:
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=text,reply_markup=markup)
             
-        if call.data == "account":
+        if call.data[:7] == "account":
+            if not call.data[7:8] == "#":
+                bot.answer_callback_query(call.id)
+            edit_btn = types.InlineKeyboardButton(text="Изменить",callback_data="edit")
+            markup.add(edit_btn,detalizated_vpns_btn)
+            markup.add(main_btn)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, 
+                text="Вы - '"+str(call.message.chat.id)+"'\n"
+                "Ваш баланс - "+str(db.getUser("userid",call.message.chat.id)[0][7]/10)+"₽\n"
+                "Текушие активные аккаунты: \n"
+                ""+getvpnstr(call.message.chat.id),reply_markup=markup)
+        
+        if call.data == "detalizated_account":
             bot.answer_callback_query(call.id)
             edit_btn = types.InlineKeyboardButton(text="Изменить",callback_data="edit")
             markup.add(edit_btn)
@@ -107,28 +195,30 @@ def callback_inline(call):
                 text="Вы - '"+str(call.message.chat.id)+"'\n"
                 "Ваш баланс - "+str(db.getUser("userid",call.message.chat.id)[0][7]/10)+"₽\n"
                 "Текушие активные аккаунты: \n"
-                ""+getvpnstr(call.message.chat.id),reply_markup=markup)
+                ""+getvpnstr(call.message.chat.id,True),reply_markup=markup)
+
         if call.data == "edit":
             bot.answer_callback_query(call.id)
             BTNmarkup = types.ReplyKeyboardMarkup(True,True)
             #BTNmarkup.row("Профиль","VPN")
-            BTNmarkup.row("VPN")
+            BTNmarkup.add("VPN")
+            BTNmarkup.add("Выйти")
             msg = bot.send_message(call.message.chat.id,"Давайте определимся, что вы хотите изменить:",reply_markup=BTNmarkup)
             bot.register_next_step_handler(msg,after_edit)
 
-        if call.data == "vpn":
-            bot.answer_callback_query(call.id)
+        if call.data[:3] == "vpn":
+            if not call.data[3:4] == '#':
+                bot.answer_callback_query(call.id)
             item1 = types.InlineKeyboardButton(text="Создать",callback_data="create_vpn")
-            markup.add(item1)
-            markup.add(destroy_btn)
-            bot.send_message(call.message.chat.id, "Выберите интересующие варианты:",reply_markup=markup)
+            item2 = types.InlineKeyboardButton(text="Мои VPN", callback_data="detalizated_account")
+            markup.add(item1,item2)
+            markup.add(main_btn)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Выберите интересующие варианты:",reply_markup=markup)
         if call.data[:10] == "create_vpn":
             if(not call.data[10:11] == "#"):
                 bot.answer_callback_query(call.id)
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Я вас понял!")
-            else:
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Я вас понял!")
-            markup.add(destroy_btn)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Я вас понял!")
+            markup.add(main_btn)
 
             
             _user = db.getUser("userid",str(call.message.chat.id))[0]
@@ -141,11 +231,12 @@ def callback_inline(call):
                 #user = str(call.message.chat.id)
                 user = str(call.message.chat.id)+''.join(secrets.choice(string.digits) for i in range(10))
                 password = ''.join(secrets.choice(alphabet) for i in range(20))  # for a 20-character password
-                protocol = "'IPSec Xauth PSK' / 'L2TP/IPSec PSK'"
+                # protocol = "'IPSec Xauth PSK' / 'L2TP/IPSec PSK'"
+                protocol = "'IPSec Xauth PSK'"
                 _ip = vpnconfig.ip
-                _time = datetime.datetime.now()+datetime.timedelta(hours=trialHours)
+                _time = datetime.datetime.now()+datetime.timedelta(hours=config.trialHours)
                 #db
-                id = db.addVpn(protocol,user,password,ip,_time.strftime('%Y-%m-%d %H:%M:%S'))
+                id = db.addVpn(protocol,user,password,vpnconfig.ip,_time.strftime('%Y-%m-%d %H:%M:%S'))
                 _json["vpnid"].append(id)
                 db.updateUser(str(call.message.chat.id),"vpnids",str(json.dumps(_json)))
                 #create vpn user
@@ -168,7 +259,7 @@ def callback_inline(call):
             BTNmarkup = types.ReplyKeyboardMarkup(True)
             BTNmarkup.row("Да","Нет")
             if(call.data[4:] == "ammount"):
-                msg = bot.send_message(call.message.chat.id,"вы хотите купить слот впн за {}₽?".format(price_slot),reply_markup=BTNmarkup)
+                msg = bot.send_message(call.message.chat.id,"вы хотите купить слот впн за {}₽?".format(config.price_slot),reply_markup=BTNmarkup)
                 bot.register_next_step_handler(msg,after_buy_ammount)
         if call.data == "destroy":
             bot.answer_callback_query(call.id)
@@ -217,8 +308,9 @@ def callback_inline(call):
                     no_btn = types.InlineKeyboardButton(text="Нет",callback_data="vpn_rendone no")
                     markup.add(yes_btn,no_btn)
                     #data = "userid="+str(call.message.chat.id)+"&vpnid="+str(user_data[call.message.chat.id]['buy']['vpnid'])+"&price="+str(user_data[call.message.chat.id]['buy']['price'])+"&extension="+str(user_data[call.message.chat.id]['buy']['extension']['raw'])
-                    bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,
-                        text="Вы хотите продлить vpn на "+user_data[call.message.chat.id]['buy']['extension']['value']+"?\n"
+                    bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text=""+user_data[call.message.chat.id]['buy']['price']+"₽")
+                    bot.send_message(call.message.chat.id,
+                        "Вы хотите продлить vpn на "+user_data[call.message.chat.id]['buy']['extension']['value']+"?\n"
                         "С вашего баланса будет списано "+user_data[call.message.chat.id]['buy']['price']+"₽"
                         ,reply_markup=markup
                     )
@@ -257,6 +349,7 @@ def callback_inline(call):
                         bot.send_message(call.message.chat.id,"Что-то сломалося. Попробуйте позже")
                     bot.answer_callback_query(call.id)
                 if call.data[12:] == "no":
+                    markup.add(main_btn)
                     bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="Оплата отменена!",reply_markup=markup)
                 user_data[call.message.chat.id]['buy'].clear()
         if call.data[:6] == "refill":
@@ -265,9 +358,10 @@ def callback_inline(call):
             BTNmarkup.row("300","500","1000")
             if(call.data[7:].isdigit()):
                 BTNmarkup.row(str(int(call.data[7:])/10))
-            msg = bot.send_message(call.message.chat.id,"Пополнить баланс на какую сумму?",reply_markup=BTNmarkup)
+            msg = bot.send_message(call.message.chat.id,"Пополнить баланс на какую сумму?\nЧтобы выйти напишите 0",reply_markup=BTNmarkup)
             bot.register_next_step_handler(msg,after_refill)
-            bot.answer_callback_query(call.id)
+            if(not call.data[6:7] == "#"):
+                bot.answer_callback_query(call.id)
 
     # Если сообщение из инлайн-режима
     elif call.inline_message_id:
@@ -295,16 +389,33 @@ def after_start(message):
         bot.send_message(message.chat.id,"Вы отказываетесь соглашаться с правилами. Тогда немедленно отчистите чат и отключите у себя бота(кнопока с ботом и кружочком перечеркнутым). Продолжить без соглашения не получиться!")
 
 def after_edit(message):
-    if(message.text == "VPN"):
+    if(message.text.lower() == "vpn"):
         
-        msg = bot.send_message(message.chat.id,"Хорошо, давай поменяем настройки vpn. Пожалуйста, укажи номер меняемого vpn:",reply_markup=types.ReplyKeyboardRemove())
+        msg = bot.send_message(message.chat.id,"Хорошо, давай поменяем настройки vpn. Пожалуйста, укажи номер меняемого vpn(Чтобы выйти, напиши 0):",reply_markup=types.ReplyKeyboardRemove())
         bot.send_message(message.chat.id,""+getvpnstr(message.chat.id))
         bot.register_next_step_handler(msg,after_edit_vpn)
-    if(message.text == "Профиль"):
+    if(message.text.lower() == "профиль"):
         bot.send_message(message.chat.id,"Да, замечательно. Давай тогда кратко пробежимся о тебе:")
+    if(message.text.lower() == "выйти"):
+        bot.send_message(message.chat.id,"Вы вышли",reply_markup=types.ReplyKeyboardRemove())
+        msg = bot.send_message(message.chat.id,"*menu*")
+        class XClass(object):
+            def __init__(self):
+                self.message = msg
+                self.data = 'main#'
+        callback_inline(XClass())
 
 def after_edit_vpn(message):
     if(message.text.isdigit()):
+        if(int(message.text) == 0):
+            bot.send_message(message.chat.id,"Вы вышли",reply_markup=types.ReplyKeyboardRemove())
+            msg = bot.send_message(message.chat.id,"*menu*")
+            class XClass(object):
+                def __init__(self):
+                    self.message = msg
+                    self.data = 'main#'
+            callback_inline(XClass())
+            return 0
         try:
             data = db.getUser("userid",message.chat.id)[0]
             jsonfromdata = json.loads(data[9])
@@ -330,6 +441,7 @@ def after_edit_vpn2(message,arg):
         user_data[message.chat.id]['buy']["vpnid"] = str(arg)
         markup = types.ReplyKeyboardMarkup(True)
         markup.row("Продлить")
+        markup.row("Выйти")
         #markup.row("Сменить пароль","Сменить логин")
         #markup.row("Активировать","Деактивировать")
         msg = bot.send_message(message.chat.id,"Хорошо, давайте дальше. Чего вы хотите?",reply_markup=markup)
@@ -351,16 +463,24 @@ def after_edit_vpn3(message):
         callback_inline(XClass())
     if(message.text.lower() == "удалить"):
         bot.send_message(message.chat.id,"Вы уверены что хотите удалить?")
+    if(message.text.lower() == "выйти"):
+        bot.send_message(message.chat.id,"Вы вышли",reply_markup=types.ReplyKeyboardRemove())
+        msg = bot.send_message(message.chat.id,"*menu*")
+        class XClass(object):
+            def __init__(self):
+                self.message = msg
+                self.data = 'main#'
+        callback_inline(XClass())
 
 def after_buy_ammount(message):
     markup = types.InlineKeyboardMarkup()
     if(message.text.lower() == "да"):
         try:
             balance = int(db.getUser("userid",message.chat.id)[0][7])
-            if(balance >= price_slot*10):
+            if(balance >= config.price_slot*10):
                 _ammount = db.cur.execute("SELECT ammount FROM users WHERE userid = ?",(message.chat.id,)).fetchall()[0][0]
                 db.updateUser(message.chat.id,"ammount",_ammount+1)
-                db.updateUser(message.chat.id,"balance",balance-(price_slot*10))
+                db.updateUser(message.chat.id,"balance",balance-(config.price_slot*10))
                 msg = bot.send_message(message.chat.id,".")
                 class XClass(object):
                     def __init__(self):
@@ -382,15 +502,24 @@ def after_refill(message):
     main_btn = types.InlineKeyboardButton(text="В главное меню",callback_data="main")
     markup.add(main_btn)
     if(is_digit(message.text)):
+        if(int(message.text)==0):
+            bot.send_message(message.chat.id,"Вы вышли",reply_markup=types.ReplyKeyboardRemove())
+            msg = bot.send_message(message.chat.id,"*menu*")
+            class XClass(object):
+                def __init__(self):
+                    self.message = msg
+                    self.data = 'main#'
+            callback_inline(XClass())
+            return 0
         balance = int(db.getUser("userid",message.chat.id)[0][7])
         if(isReal_payment):
             price = [types.LabeledPrice(label="Руб",amount=int(float(int(float(message.text)*10)/10)*100))]
             bot.send_invoice(
                 message.chat.id,        #chat_id
-                "Оплата",              #title
+                "Оплата",               #title
                 "Пополнение баланса",   #description
                 "refill",               #payload
-                pay_token, #token
+                config.pay_token,       #token
                 "RUB",                  #curency
                 price,                  #price
                 "test"                  #start_parameter
